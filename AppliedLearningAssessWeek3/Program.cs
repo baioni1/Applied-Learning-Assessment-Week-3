f8d76500320b6c15d5ef598f52ab628e65481ae1
Your Name <your@example.com>
2016-03-14 18:25:15 -0400

    Applied Learning Assessment

---
+++ AppliedLearningAssessWeek3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedLearningAssessWeek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assessment 1
+
+            //ask user for an integer
+            /*Console.WriteLine("Pick an integer:");
+            //parse user number/Readline
+            int userInput = int.Parse(Console.ReadLine());
+            //if number is <10 print "This number is too small"
+            if (userInput < 10)
+            {
+                Console.WriteLine("This number is too small.");
+            }
+            //if number is >=10 print "This number is big enough"
+            else
+            {
+                Console.WriteLine("This number is big enough.");
+            }*/
+
+
+            //Assessment 2  
+
+            //intitialize an array of floating -point values i.e (float, double, decimal) with five values
+            /*double[] nums = { .666, .667, .668, .669, .700 };
+            // print out the second value only
+            Console.WriteLine(nums[1]);*/
+
+
+            //Assessment 3
+
+            //intitialize an array of floating -point values i.e (float, double, decimal) with five values
+            /*double[] nums = { 1.66, 2.45, 2.68, 1.93, 4.72 }; 
+            //use foreach loop
+            foreach(double num in nums)
+            {
+                Console.Write(" "+num+" ");
+            }*/
+
+
+            //Assessment 4 
+
+            //ask user to input an integer
+            /*Console.WriteLine("Pick an integer:");
+            //parse user input/readline
+            int userInput = int.Parse(Console.ReadLine());
+            //Use Switch case
+            switch (userInput)
+            {
+                case 1:// if user picks 1, print "Only one?"
+                    Console.WriteLine("Only one?");
+                    break;
+                case 100:// if user picks 100, print "100? That's a lot!"
+                    Console.WriteLine("100? That's a lot!");
+                    break;
+                default:// all others (default) "Input not recognized."
+                    Console.WriteLine("Input not recognized.");
+                    break;
+            }*/
+            
+            
+            
+            //all other numbers (default
+
+
+        }
+    }
+}
